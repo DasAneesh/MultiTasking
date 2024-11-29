f2ff48eb91ed379b1ddf1e77463329439ea2af50
DasAneesh <149708161+DasAneesh@users.noreply.github.com>
2024-11-29 21:00:46 +0300

    new commits

---
+++ Multitasking/ConsoleApp1/ListofNumbers.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ListofNumbers
+    {
+        private List<int> Numbers;
+
+        public ListofNumbers()
+        {
+            Numbers = new List<int>();
+        }
+
+        public int CalculateSimplesum()
+        {
+            int sum = 0;
+            foreach (int i in Numbers)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public int CalculateParallelsum(int Threads)
+        {
+
+            List<int> list = new List<int>();
+            Task[] tasks1 = new Task[Threads];
+
+
+            for (int i = 0; i < Threads; i++)
+            {
+                tasks1[i] = new Task(() =>
+                {
+                    list.Add(0);
+                    for (int j = i * (Numbers.Count() / Threads); j < (i + 1) * (Numbers.Count() / Threads); j++)
+                    {
+                        list[i] += Numbers[j];
+
+                    }
+                });
+
+            }
+
+            foreach (var t in tasks1)
+                t.Start();
+
+            Task.WaitAll(tasks1);
+            return list.Sum();
+        }
+        public void AddNumbers(int lenght)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < lenght; i++)
+            {
+                Numbers.Add(rnd.Next(1, 1000));
+            }
+        }
+        public List<int> GetNumbers()
+        {
+            return this.Numbers;
+        }
+
+
+
+
+    }
+}
---
+++ Multitasking/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListofNumbers ints = new ListofNumbers();
+            ints.AddNumbers(20000);
+            Console.WriteLine(ints.CalculateParallelsum(5));
+            Console.ReadLine();
+        }
+    }
+}
